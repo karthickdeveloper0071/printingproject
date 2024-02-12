80ddbe0868bc6725a49809503909b5f951a4425d
karthickdeveloper0071 <148254721+karthickdeveloper0071@users.noreply.github.com>
2024-02-12 13:24:21 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,5 +1,19 @@
 <?php 
-
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$email_from = 'https://karthickdeveloper0071.github.io/printingproject/contact.html'; I
+$email_subject 'New Form Submission';
+$email_body = "User Name: $name.\n".
+"User Email: $visitor_email.\n".
+"Subject: $subject.\n".
+"User Message: $message .\n";
+$to = 'karthick123svks@gmail.com';
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+mail($to, $email_subject, $email_body,$headers);
+header("Location: contact.html");
 
 
 ?>
